0d2f846c904efe06887e3b52dfddf27c4ae9d512
苔コッコー <158864238+Kokecoco@users.noreply.github.com>
2024-10-13 17:34:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,12 @@ app.get("/api/get-info", async (req, res) => {
   }
 
   try {
-    const response = await axios.get(url);
+    // User-Agent ヘッダーを追加
+    const response = await axios.get(url, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+      }
+    });
     const $ = cheerio.load(response.data);
 
     // ページのタイトルを取得
